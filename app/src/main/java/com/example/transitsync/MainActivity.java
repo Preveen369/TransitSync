080716bb7c90cb467e9d3dd4ada3dd21ab2462f8
Preveen369 <spreveen123@gmail.com>
2024-10-08 20:24:56 +0530

    Added main activity

--- app/src/main/java/com/example/transitsync/MainActivity.java
+++ app/src/main/java/com/example/transitsync/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.transitsync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button busLocation, busDetails, profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        busLocation = findViewById(R.id.track_bus_location);
+        busDetails = findViewById(R.id.view_bus_details);
+        profile = findViewById(R.id.profile);
+
+        busLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BusLocation.class);
+                startActivity(intent);
+            }
+        });
+
+        busDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BusDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
